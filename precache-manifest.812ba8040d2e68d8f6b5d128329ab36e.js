93fae79a5bda91d0f1b0be0e4dac8f463512610b
caoleyun <244814637@qq.com>
2019-05-22 21:06:53 +0800

    部署到分支

---
+++ precache-manifest.812ba8040d2e68d8f6b5d128329ab36e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7017a5a4de98825f1f17",
+    "url": "/static/js/main.c8926d71.chunk.js"
+  },
+  {
+    "revision": "474ab9d4ae743fe23243",
+    "url": "/static/js/2.79d17c87.chunk.js"
+  },
+  {
+    "revision": "7017a5a4de98825f1f17",
+    "url": "/static/css/main.f213c9d4.chunk.css"
+  },
+  {
+    "revision": "0afb5702e668d19f626ac5a38486fdf8",
+    "url": "/index.html"
+  }
+];
